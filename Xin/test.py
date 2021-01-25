9576af77dd5a384c692316371d73878da6d57ee1
WOWO5 <2846941483@qq.com>
2021-01-25 11:29:14 +0800

    push from WOWO

--- Xin/test.py
+++ Xin/test.py
@@ -1,8 +1,26 @@
-import random
+import turtle
+from random import randint
 
-count = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0}
+def draw_star():
+  turtle.color('black')
+  turtle.hideturtle()
+  turtle.begin_fill()
+  for i in range(5):
+    turtle.forward(10)
+    turtle.right(144)
+  turtle.end_fill()
 
-for i in range(1000):
-    choice = random.choice(list(count.keys()))
-    count[choice] += 1
-print(count)
+for i in range(50):
+  turtle.speed(0)
+  turtle.penup()
+  x = randint(-150, 150)
+  y = randint(-100, 100)
+  turtle.goto(x, y)
+  turtle.pendown()
+  draw_star()
+
+turtle.penup()
+turtle.goto(0, -130)
+turtle.pendown()
+turtle.write('By 黄帮主',  font = ('SimHei', 12, 'bold'))
+turtle.done()
