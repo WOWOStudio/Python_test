d04a583aeb57d08d8b17425a2cbfcdedcf600b72
WOWO5 <2846941483@qq.com>
2021-01-27 09:41:15 +0800

    push from WOWO5

--- Xin/test.py
+++ Xin/test.py
@@ -1,26 +1,4 @@
-import turtle
-from random import randint
-
-def draw_star():
-  turtle.color('black')
-  turtle.hideturtle()
-  turtle.begin_fill()
-  for i in range(5):
-    turtle.forward(10)
-    turtle.right(144)
-  turtle.end_fill()
-
-for i in range(10):
-  turtle.speed(0)
-  turtle.penup()
-  x = randint(-150, 150)
-  y = randint(-100, 100)
-  turtle.goto(x, y)
-  turtle.pendown()
-  draw_star()
-
-turtle.penup()
-turtle.goto(0, -130)
-turtle.pendown()
-turtle.write('By 黄帮主',  font = ('SimHei', 12, 'bold'))
-turtle.exitonclick()
+import requests
+res = requests.get('http://www.wowo7.xyz')
+with open('WOWO.txt','w') as file:
+    file.write(res.text)
