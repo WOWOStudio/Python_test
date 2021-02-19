e00d8c5fd00ad7d362ad107f729d4a27feb4f57b
WOWO5 <2846941483@qq.com>
2021-02-19 10:05:22 +0800

    WOWO5

---
+++ Xin/LeetCode/data_structure/chapterOne/sectionFour.py
@@ -0,0 +1,43 @@
+'''
+剑指 Offer 09. 用两个栈实现队列
+用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead操作返回 -1 )
+
+示例 1：
+
+输入：
+["CQueue","appendTail","deleteHead","deleteHead"]
+[[],[3],[],[]]
+输出：[null,null,3,-1]
+示例 2：
+
+输入：
+["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+[[],[],[5],[2],[],[]]
+输出：[null,-1,null,null,5,2]
+提示：
+
+1 <= values <= 10000
+最多会对appendTail、deleteHead 进行10000次调用
+
+作者：Krahets
+链接：https://leetcode-cn.com/leetbook/read/illustration-of-algorithm/5d3i87/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+'''
+
+class CQueue:
+    def __init__(self):
+        self.A, self.B = [], []
+
+    def appendTail(self, value: int) -> None:
+        self.A.append(value)
+
+    def deleteHead(self) -> int:
+        if self.B:
+            return self.B.pop()
+        if not self.A:
+            return -1
+        while self.A:
+            self.B.append(self.A.pop())
+        return self.B.pop()
